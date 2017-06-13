dfee4d003cec01db4fd5f8da8577934178e7d679
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-06-13 15:49:38 -0600

    Turning result cards into their own component

---
+++ client/src/components/PollResults.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="header clearfix">
+        <span>{{poll.name}}</span>
+        <!--<i v-if="isOwner(poll) || this.userRole === 'admin'" class="el-icon-delete icon" @click="deletePoll(poll)"></i>-->
+        <!--<i v-if="isOwner(poll) || this.userRole === 'admin'" class="el-icon-edit icon" @click="editPoll(poll)"></i>-->
+      </div>
+      <div class="body">
+        <div v-for="option in poll.options">
+          {{option.option}} - {{option.votes.length}} Votes
+          <el-progress :text-inside="true" :stroke-width="18" :percentage="percentOfVotes(option.votes.length, poll.totalVotes)"></el-progress>
+        </div>
+        {{poll.totalVotes || 0}} Total Votes
+        <h3>Poll by {{poll.author}}</h3>
+      </div>
+      <el-button size="small" @click="goTo('/polls/' + poll._id)" type="primary">Vote</el-button>
+    </el-card>
+  </div>
+</template>
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters([
+        'username',
+        'userRole'
+      ])
+    },
+    methods: {
+      goTo (route) {
+        this.$router.push(route)
+      },
+      isOwner (poll) {
+        return poll.author === this.username
+      },
+      percentOfVotes (votes, total) {
+        const percent = (votes / total) * 100
+        if (!total) return 0
+        if (percent % 1 === 0) return percent
+        return Number(percent.toFixed(2))
+      }
+    },
+    props: ['poll']
+  }
+</script>
+<style lang="stylus" scoped>
+  @import "../assets/css/variable"
+  .item
+    padding: 5px 0
+  .box-card
+    width 20rem
+    margin 1rem
+    h2
+      16px
+      font-weight normal
+    .icon
+      float right
+      margin-left .5rem
+      cursor pointer
+      &:hover
+        color $color-primary
+    .header
+      color #0cb4e8
+      font-weight bold
+      font-size 20px
+  .ownPoll
+    background-color #232323
+    div
+      color: white
+</style>
--- client/src/view/Dashboard.vue
+++ client/src/view/Dashboard.vue
@@ -1,10 +1,13 @@
 <template>
   <content-module name="dashboard">
-
+    From Dashboard {{polls}}
+    From Component
+    <poll-results v-for="(poll, index) in this.polls" :key="index" :poll="poll"></poll-results>
   </content-module>
 </template>
 <script>
   import { poll as pollRes } from 'resources'
+  import PollResults from 'components/PollResults'
   export default {
     // todo
     data: function () {
@@ -20,6 +23,9 @@
           console.error(err)
         })
       }
+    },
+    components: {
+      PollResults
     },
     created () {
       this.fetch()
