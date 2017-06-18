9d94881d7d68190ecb3847c1426ce843d846b85a
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-06-18 00:50:55 -0600

    Created page for my polls

--- client/src/router/module.js
+++ client/src/router/module.js
@@ -26,4 +26,9 @@ export default [{
   component: (resolve) => {
     import('../view/SinglePoll.vue').then(resolve)
   }
+}, {
+  path: '/me',
+  component: (resolve) => {
+    import('../view/MyPolls.vue').then(resolve)
+  }
 }]
---
+++ client/src/view/MyPolls.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Hello {{username}}!
+  </div>
+</template>
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['username'])
+    },
+    methods: {
+      fetch () {
+        console.log('Grabbing ' + this.username + '\'s polls')
+      }
+    },
+    created () {
+      this.fetch()
+    }
+  }
+</script>
+<style></style>
