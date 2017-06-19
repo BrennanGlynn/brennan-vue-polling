5011a0149a9f696ca0b8da8e022b6b1cfac2d300
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-06-18 18:56:48 -0600

    ''

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-require('path')
-const serveStatic = require('serve-static')
-const app = express()
-app.use(serveStatic(__dirname))
-const port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
